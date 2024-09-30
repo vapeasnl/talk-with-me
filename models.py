d4dc97e90e0249cd83359789f50468b2be21f23b
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-09-30 11:57:50 +0200

    Create models.py

---
+++ models.py
@@ -0,0 +1,31 @@
+from db import db  # Importer db depuis db.py
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    hash = db.Column(db.String(128), nullable=False)
+
+    stories = db.relationship('Story', backref='author', lazy=True)
+
+class Story(db.Model):
+    __tablename__ = 'stories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    likes = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    comments = db.relationship('Comment', backref='story', lazy=True)
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    likes = db.Column(db.Integer, default=0)
+    story_id = db.Column(db.Integer, db.ForeignKey('stories.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
