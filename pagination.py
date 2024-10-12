7642d1d713bcc4d6a6fef7912628494cdd625ee4
Abdessamad SALIM <106477450+vapeasnl@users.noreply.github.com>
2024-10-12 13:06:37 +0200

    Update pagination.py

--- pagination.py
+++ pagination.py
@@ -22,7 +22,7 @@ class Pagination(object):
     def iter_pages(self, left_edge=2, left_current=2,
                    right_current=5, right_edge=2):
         last = 0
-        for num in range(1, self.pages + 1):  # Remplacement de xrange par range
+        for num in range(1, self.pages + 1):
             if num <= left_edge or \
                (num > self.page - left_current - 1 and \
                 num < self.page + right_current) or \
